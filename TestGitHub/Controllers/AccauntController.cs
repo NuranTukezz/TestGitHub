9d6624658580ee36699208a49194fc5b7d7b135e
NuranTukezz <104999477+NuranTukezz@users.noreply.github.com>
2022-06-12 17:12:26 +0300

    Login_Branch eklendi Accaunt Controller Index,Login ve Register html leri eklendi

---
+++ TestGitHub/Controllers/AccauntController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestGitHub.Controllers
+{
+    public class AccauntController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
